3e08c5aacfdf58b72b460847d19663c420da17bc
Manser <msarreygue@gmail.com>
2025-10-16 11:04:32 -0600

    se cambian cosas escenciales

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/aquacaretrack/', // ← Esta línea es crucial
   build: {
     outDir: 'dist',
+    assetsDir: 'assets'
   },
   define: {
     // Fix: Expose process.env.API_KEY to client-side code for Gemini API compatibility.
